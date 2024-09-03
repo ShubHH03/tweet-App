d8eb4199ba9336eacb99d09669000f68335ffefa
ShubHH03 <mak.shubham2003@gmail.com>
2024-09-03 17:32:34 +0530

    final changes

--- tweetApp/forms.py
+++ tweetApp/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 from .models import Tweet
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields = ['text', 'photo']
-        # widgets = {
-        #     'text': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Tweet something...'}),
-        # }
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
